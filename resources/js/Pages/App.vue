54f9ddad18f650795026a48740771df1ca83d359
alexxxey <aleksejs.gorlovics@gmail.com>
2023-07-04 21:27:16 +0300

    initialize

---
+++ resources/js/Pages/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello Vue</h1>
+</template>
+
+<script>
+    export default {
+        name: 'App'
+    }
+</script>
+
+<style>
+    h1 {
+        color: blue;
+        border: 5px solid green;
+    }
+</style>
